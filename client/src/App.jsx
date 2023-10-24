4be0732b4c568f3cbd3e5dfc7bf2fa9addd2b703
Bryan <34315902+bryan-padero@users.noreply.github.com>
2023-10-24 13:30:53 +0400

    added client pages and css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="main">
+      <main>Home</main>
+    </div>
+  );
+}
+
+export default Home;
